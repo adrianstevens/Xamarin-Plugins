f9480c11e8592dfa6a6d9e5f415ac96ed9e64c80
Adrian Stevens <adrian.stevens@gmail.com>
2018-04-04 18:56:14 -0700

    Removed incomplete secure local storage project - there's a better one in development :)

--- SecureLocalStorage/clean.bat
+++
@@ -1,3 +0,0 @@
-FOR /F "tokens=*" %%G IN ('DIR /B /AD /S bin') DO RMDIR /S /Q "%%G"
-FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
-FOR /F "tokens=*" %%G IN ('DIR /B /AD /S packages') DO RMDIR /S /Q "%%G"
